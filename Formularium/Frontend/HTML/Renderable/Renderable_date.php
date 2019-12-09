0b0ed1fd4e193a5393551857b1b84bc8fbf3f520
Bruno <brunobg@gmail.com>
2019-12-09 17:24:12 -0300

    fixes generator html, bulma

--- Formularium/Frontend/HTML/Renderable/Renderable_date.php
+++ Formularium/Frontend/HTML/Renderable/Renderable_date.php
@@ -2,16 +2,15 @@
 
 namespace Formularium\Frontend\HTML\Renderable;
 
-use Formularium\Datatype\DataType_uuid;
 use Formularium\Field;
 use Formularium\HTMLElement;
 
-class Renderable_uuid extends Renderable_string
+class Renderable_date extends Renderable_string
 {
     public function editable($value, Field $field, HTMLElement $previous): HTMLElement
     {
         $element = parent::editable($value, $field, $previous);
-        $element->get('input')[0]->setAttribute('pattern', DataType_uuid::UUID_REGEX);
+        $element->get('input')[0]->setAttribute('type', 'date');
         return $element;
     }
 }
