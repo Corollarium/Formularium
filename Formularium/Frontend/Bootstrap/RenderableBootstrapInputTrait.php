488614f7e8f8d7a60dce5823f375b109fa904e9a
Bruno <brunobg@gmail.com>
2019-12-10 12:36:39 -0300

    phpstan review

--- Formularium/Frontend/Bootstrap/RenderableBootstrapInputTrait.php
+++ Formularium/Frontend/Bootstrap/RenderableBootstrapInputTrait.php
@@ -14,6 +14,14 @@ trait RenderableBootstrapInputTrait
         return $previous;
     }
 
+    /**
+     * Subcall of wrapper editable() from RenderableMaterializeTrait
+     *
+     * @param mixed $value
+     * @param Field $field
+     * @param HTMLElement $previous
+     * @return HTMLElement
+     */
     public function _editable($value, Field $field, HTMLElement $previous): HTMLElement
     {
         // add extra classes
