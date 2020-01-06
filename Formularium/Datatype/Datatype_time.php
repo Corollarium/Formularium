a786b7ad12127d1fd5e20e930c406eee696a0783
Bruno <brunobg@gmail.com>
2020-01-06 15:41:48 -0300

    Formularium/Datatype/Datatype_time: create fix #9

---
+++ Formularium/Datatype/Datatype_time.php
@@ -0,0 +1,42 @@
+<?php declare(strict_types=1);
+
+namespace Formularium\Datatype;
+
+use Formularium\Field;
+use Formularium\Exception\ValidatorException;
+use Respect\Validation\Validator;
+
+class Datatype_time extends \Formularium\Datatype
+{
+    public function __construct(string $typename = 'time', string $basetype = 'time')
+    {
+        parent::__construct($typename, $basetype);
+    }
+
+    public function getRandom(array $params = [])
+    {
+        $faker = static::faker();
+        return $faker->time();
+    }
+
+    public function validate($value, Field $f)
+    {
+        if ($value === '') {
+            return $value;
+        }
+
+        $matches = [];
+        if (preg_match("/^(\d{2}):(\d{2})(:(\d{2}))?$/", $value, $matches) === 1) {
+            if (count($matches) == 3) {
+                $matches[3] = 0;
+            }
+            if ($matches[1] < 24 && $matches[2] < 60 && $matches[3] < 60) {
+                return $value;
+            }
+        }
+
+        throw new ValidatorException(
+            'Invalid time value. We expect this format: HH:MM:SS. ' . print_r($value, true)
+        );
+    }
+}
