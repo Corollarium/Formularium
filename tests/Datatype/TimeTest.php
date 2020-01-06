3b5c3fc3a69fc08f18762d089b2c7b4c9528e1b0
Bruno <brunobg@gmail.com>
2020-01-06 15:42:09 -0300

    test time

---
+++ tests/Datatype/TimeTest.php
@@ -0,0 +1,43 @@
+<?php declare(strict_types=1);
+
+require_once('DatatypeBaseTestCase.php');
+
+use Formularium\Datatype;
+
+class DatatypeTime_TestCase extends DatatypeBaseTestCase
+{
+
+    /**
+     * @return DataType
+     */
+    public function getDataType(): \Formularium\Datatype
+    {
+        return \Formularium\Datatype::factory('time');
+    }
+
+    /**
+     * @return array
+     */
+    public function getValidValues()
+    {
+        return [
+            '12:30:45',
+            '09:20',
+            '16:40',
+            ''
+        ];
+    }
+
+    /**
+     * @return array
+     */
+    public function getInvalidValues()
+    {
+        return [
+            '26:30:10',
+            'asdfa',
+            '2012-25-01',
+            '2001-00-00'
+        ];
+    }
+}
