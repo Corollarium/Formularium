04f94bdebc62021719c0df72d54a66d6c9ef5188
Bruno <brunobg@gmail.com>
2020-01-07 14:51:19 -0300

    alpha datatypes

---
+++ Formularium/Datatype/Datatype_alpha.php
@@ -0,0 +1,34 @@
+<?php declare(strict_types=1);
+
+namespace Formularium\Datatype;
+
+use Formularium\Field;
+use Formularium\Model;
+use Formularium\Exception\ValidatorException;
+
+/**
+ * Validates value contains only alphabetic characters.
+ */
+class Datatype_alpha extends \Formularium\Datatype\Datatype_string
+{
+    public function __construct(string $typename = 'alpha', string $basetype = 'string')
+    {
+        parent::__construct($typename, $basetype);
+    }
+
+    public function getRandom(array $params = [])
+    {
+        $min = $params[static::MIN_LENGTH] ?? 5;
+        $max = $params[static::MAX_LENGTH] ?? 15;
+        return static::getRandomString($min, $max, "abcdefghijkmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ");
+    }
+
+    public function validate($value, Field $field, Model $model = null)
+    {
+        $value = parent::validate($value, $field, $model);
+        if (!preg_match('/^[\pL\pM]+$/u', $value)) {
+            throw new ValidatorException('Use only alphabetic characters.');
+        }
+        return $value;
+    }
+}
---
+++ Formularium/Datatype/Datatype_alphanumdash.php
@@ -0,0 +1,34 @@
+<?php declare(strict_types=1);
+
+namespace Formularium\Datatype;
+
+use Formularium\Field;
+use Formularium\Model;
+use Formularium\Exception\ValidatorException;
+
+/**
+ * Validates value contains only alpha-numeric characters, dashes, and underscores.
+ */
+class Datatype_alphanumdash extends \Formularium\Datatype\Datatype_string
+{
+    public function __construct(string $typename = 'alphanumdash', string $basetype = 'string')
+    {
+        parent::__construct($typename, $basetype);
+    }
+
+    public function getRandom(array $params = [])
+    {
+        $min = $params[static::MIN_LENGTH] ?? 5;
+        $max = $params[static::MAX_LENGTH] ?? 15;
+        return static::getRandomString($min, $max, "abcdefghijkmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789_-");
+    }
+
+    public function validate($value, Field $field, Model $model = null)
+    {
+        $value = parent::validate($value, $field, $model);
+        if (!preg_match('/^[\pL\pM\pN_-]+$/u', $value)) {
+            throw new ValidatorException('Use only alpha-numeric characters, dashes, and underscores.');
+        }
+        return $value;
+    }
+}
