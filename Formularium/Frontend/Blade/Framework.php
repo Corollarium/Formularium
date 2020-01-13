1b304f2d8f523412f7ce99c7a0a8cdb5822f5018
Bruno <brunobg@gmail.com>
2020-01-13 11:43:45 -0300

    starting blade frontend

---
+++ Formularium/Frontend/Blade/Framework.php
@@ -0,0 +1,73 @@
+<?php declare(strict_types=1);
+
+namespace Formularium\Frontend\Blade;
+
+use Formularium\HTMLElement;
+use Formularium\Model;
+
+class Framework extends \Formularium\Framework
+{
+    /**
+     * The viewable template.
+     *
+     * @var string
+     */
+    protected $viewableTemplate = '';
+
+    /**
+     * @var string
+     */
+    protected $editableTemplate = '';
+
+    public function __construct(string $name = 'Blade')
+    {
+        parent::__construct($name);
+
+        $this->editableTemplate = <<<EOF
+{!! Form::open(['route' => '{{modelName}}.store']) !!}
+
+{{form}}
+
+{!! Form::submit('Submit', ['class' => 'btn btn-info']) !!}
+
+{!! Form::close() !!}
+EOF;
+    }
+
+    public function editableCompose(Model $m, array $elements, string $previousCompose): string
+    {
+        $editableForm = join('', $elements);
+        $templateData = [
+            'form' => $editableForm,
+        ];
+   
+        return $this->fillTemplate(
+            $this->editableTemplate,
+            $templateData,
+            $m
+        );
+    }
+
+    protected function fillTemplate(string $template, array $data, Model $m): string
+    {
+        foreach ($data as $name => $value) {
+            $template = str_replace(
+                '{{' . $name . '}}',
+                $value,
+                $template
+            );
+        }
+
+        $template = str_replace(
+            '{{modelName}}',
+            $m->getName(),
+            $template
+        );
+        $template = str_replace(
+            '{{modelNameLower}}',
+            mb_strtolower($m->getName()),
+            $template
+        );
+        return $template;
+    }
+}
