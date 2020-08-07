5c9a8600ab402981ee22e169a64e3512b13312d4
Bruno <brunobg@gmail.com>
2020-08-07 18:42:29 -0300

    card element

---
+++ Formularium/Frontend/Materialize/Element/Card.php
@@ -0,0 +1,86 @@
+<?php declare(strict_types=1);
+
+namespace Formularium\Frontend\Materialize\Element;
+
+use Formularium\Element;
+use Formularium\HTMLNode;
+use Formularium\Metadata;
+use Formularium\Frontend\HTML\Element\Card as HTMLCard;
+
+class Card extends Element
+{
+    public function render(array $parameters, HTMLNode $previous): HTMLNode
+    {
+        $base = HTMLNode::factory(
+            'div',
+            [
+                'class' => 'formularium-card card'
+            ]
+        );
+
+        if ($parameters[HTMLCard::IMAGE] ?? false) {
+            $image = HTMLNode::factory(
+                'div',
+                [
+                    'class' => 'card-image',
+                    'alt' => '' // TODO
+                ],
+                HTMLNode::factory(
+                    'img',
+                    [
+                        'src' => $parameters[HTMLCard::IMAGE],
+                        'alt' => '' // TODO
+                    ]
+                )
+            );
+            $base->appendContent($image);
+        }
+
+        $body = HTMLNode::factory(
+            'div',
+            [
+                'class' => 'card-content'
+            ]
+        );
+        $base->appendContent($body);
+
+        // title
+        if ($parameters[HTMLCard::TITLE] ?? false) {
+            $titleData = null;
+            if ($parameters[HTMLCard::LINK] ?? false) {
+                $titleData = HTMLNode::factory(
+                    'a',
+                    [ 'href' => $parameters[HTMLCard::LINK] ],
+                    $parameters[HTMLCard::TITLE]
+                );
+            } else {
+                $titleData = $parameters[HTMLCard::TITLE];
+            }
+            $title = HTMLNode::factory(
+                'span',
+                [
+                    'class' => 'card-title'
+                ],
+                $titleData
+            );
+            $body->appendContent($title);
+        }
+
+        if ($parameters[HTMLCard::CONTENT] ?? false) {
+            $content = HTMLNode::factory(
+                'p',
+                [
+                ],
+                $parameters[HTMLCard::CONTENT]
+            );
+            $body->appendContent($content);
+        }
+        return $base;
+    }
+
+    public static function getMetadata(): Metadata
+    {
+        $metadata = HTMLCard::getMetadata();
+        return $metadata;
+    }
+}
