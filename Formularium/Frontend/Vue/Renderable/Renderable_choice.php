d9ba4f2f06f45aab867f884149bf86a3782468af
Bruno <brunobg@gmail.com>
2020-08-10 18:22:54 -0300

    missing choice renderable

---
+++ Formularium/Frontend/Vue/Renderable/Renderable_choice.php
@@ -0,0 +1,12 @@
+<?php declare(strict_types=1);
+
+namespace Formularium\Frontend\Vue\Renderable;
+
+use Formularium\Field;
+use Formularium\Frontend\Vue\RenderableVueTrait;
+use Formularium\HTMLNode;
+
+class Renderable_choice extends \Formularium\Renderable
+{
+    use RenderableVueTrait;
+}
